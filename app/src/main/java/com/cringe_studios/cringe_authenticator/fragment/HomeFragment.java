80ae3dfad064b74da0d4c15291835299520ab96a
MrLetsplay <mr@cringe-studios.com>
2023-06-20 21:41:24 +0200

    Rename files

---
+++ app/src/main/java/com/cringe_studios/cringe_authenticator/fragment/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.cringe_studios.cringe_authenticator.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.cringe_studios.cringe_authenticator.databinding.FragmentHomeBinding;
+import com.cringe_studios.cringe_authenticator.util.FabUtil;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        FabUtil.hideFabs(getActivity());
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
