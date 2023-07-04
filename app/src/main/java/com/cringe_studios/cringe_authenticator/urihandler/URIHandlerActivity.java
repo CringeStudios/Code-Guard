cfbb969c5c08d61daf936b3885ffe08752013cad
MrLetsplay <mr@cringe-studios.com>
2023-07-04 08:12:52 +0200

    Add code import from URI handler

--- app/src/main/java/com/cringe_studios/cringe_authenticator/urihandler/URIHandlerActivity.java
+++ app/src/main/java/com/cringe_studios/cringe_authenticator/urihandler/URIHandlerActivity.java
@@ -11,7 +11,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.cringe_studios.cringe_authenticator.R;
 import com.cringe_studios.cringe_authenticator.model.OTPData;
 import com.cringe_studios.cringe_authenticator.util.OTPParser;
+import com.cringe_studios.cringe_authenticator.util.SettingsUtil;
 import com.cringe_studios.cringe_authenticator.util.StyledDialogBuilder;
+
+import java.util.List;
 
 public class URIHandlerActivity extends AppCompatActivity {
 
@@ -28,8 +31,24 @@ public class URIHandlerActivity extends AppCompatActivity {
         try {
             OTPData data = OTPParser.parse(intent.getData());
             // TODO: choose group, add code
-            Toast.makeText(this, "Code received", Toast.LENGTH_LONG).show();
-            finish();
+            List<String> groups = SettingsUtil.getGroups(this);
+            String[] groupNames = new String[groups.size()];
+            for(int i = 0; i < groups.size(); i++) {
+                groupNames[i] = SettingsUtil.getGroupName(this, groups.get(i));
+            }
+
+            // TODO: add option to create new group?
+            AlertDialog dialog = new StyledDialogBuilder(this)
+                    .setTitle(R.string.uri_handler_add_code_title)
+                    .setItems(groupNames, (d, which) -> {
+                        SettingsUtil.addOTP(this, groups.get(which), data);
+                        Toast.makeText(this, R.string.uri_handler_code_added, Toast.LENGTH_SHORT).show();
+                    })
+                    .setPositiveButton(R.string.ok, (d, which) -> finish())
+                    .create();
+
+            dialog.setOnDismissListener(d -> finish());
+            dialog.show();
         }catch(IllegalArgumentException e) {
             AlertDialog dialog = new StyledDialogBuilder(this)
                     .setTitle(R.string.uri_handler_failed_title)
