98e30b78a35cc071ad75ef47a70619944790f1ca
MrLetsplay <mr@cringe-studios.com>
2023-09-19 20:58:23 +0200

    Format codes

--- app/src/main/java/com/cringe_studios/cringe_authenticator/otplist/OTPListItem.java
+++ app/src/main/java/com/cringe_studios/cringe_authenticator/otplist/OTPListItem.java
@@ -29,4 +29,18 @@ public class OTPListItem extends RecyclerView.ViewHolder {
         return otpData;
     }
 
+    public static String formatCode(String code) {
+        // TODO: add setting for group size (and enable/disable grouping)
+        StringBuilder b = new StringBuilder();
+        for(int i = 0; i < code.length(); i++) {
+            if(i != 0 && i % 3 == 0) {
+                b.append(' ');
+            }
+
+            char c = code.charAt(i);
+            b.append(c);
+        }
+        return b.toString();
+    }
+
 }
