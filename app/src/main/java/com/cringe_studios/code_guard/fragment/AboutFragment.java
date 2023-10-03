edbc9420e1245a3228522ce3883dd4afd26538bf
MrLetsplay <mr@cringe-studios.com>
2023-10-03 14:59:20 +0200

    Show version in about, Update version, Fix links in about

--- app/src/main/java/com/cringe_studios/code_guard/fragment/AboutFragment.java
+++ app/src/main/java/com/cringe_studios/code_guard/fragment/AboutFragment.java
@@ -1,5 +1,7 @@
 package com.cringe_studios.code_guard.fragment;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +26,13 @@ public class AboutFragment extends NamedFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentAboutBinding.inflate(inflater);
+        try {
+            PackageManager manager = requireContext().getPackageManager();
+            PackageInfo info = null;
+            info = manager.getPackageInfo(requireContext().getPackageName(), 0);
+            String version = info.versionName;
+            binding.appVersion.setText(version);
+        } catch (PackageManager.NameNotFoundException ignored) {}
         return binding.getRoot();
     }
 }
