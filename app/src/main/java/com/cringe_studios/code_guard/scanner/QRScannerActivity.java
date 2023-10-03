2347f9c57e186a7c5281777f846aef41b4fad932
MrLetsplay <mr@cringe-studios.com>
2023-10-03 21:10:06 +0200

    Error when camera permission denied

--- app/src/main/java/com/cringe_studios/code_guard/scanner/QRScannerActivity.java
+++ app/src/main/java/com/cringe_studios/code_guard/scanner/QRScannerActivity.java
@@ -83,6 +83,15 @@ public class QRScannerActivity extends BaseActivity {
         scanner = new QRScanner();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1234 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            error(getString(R.string.error_no_camera_permission));
+        }
+    }
+
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
         Preview preview = new Preview.Builder()
                 .build();
